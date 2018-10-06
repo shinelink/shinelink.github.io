ef8dd8f931bee7fe05635b11eafdb13b45c443e3
shinelink <shinelink@users.noreply.github.com>
2018-10-06 13:39:02 +0800

    Add files via upload

--- utils/data2.py
+++ utils/data2.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Guess: Feather is a fast, lightweight, and easy-to-use binary file format for storing data frames. 
+
 def load_sequence_and_metadata(kind='feather'):
     """
     Returns the sequences as a list of SeqRecords, and metadata as a pandas
@@ -15,37 +16,46 @@ def load_sequence_and_metadata(kind='feather'):
     """
     
     starttime = datetime.now()
-    sequences = [s for s
-                 in SeqIO.parse('data/20170531-H3N2-global.fasta', 'fasta')                            
-                 ]
-    if kind == 'csv':
-        metadata = pd.read_csv('data/20170531-H3N2-global.tsv',        
-                               sep='\t',
-                               parse_dates=['Collection Date'])
-    elif kind == 'feather':
-        metadata = pd.read_feather('data/20170531-H3N2-global.feather')        
-    endtime = datetime.now()
-    elapsed = endtime - starttime
-    print(f'load_sequence_and_metadata() took {elapsed} seconds.')
-    return sequences, metadata
-
-
-def load_sequence_and_metadata_addH3N2(kind='feather'):
-    """
-    Returns the sequences as a list of SeqRecords, and metadata as a pandas
-    DataFrame.
-    """
     
-    starttime = datetime.now()
-    sequences = [s for s                 
-                 in SeqIO.parse('data/2000_2014_H3N2_global_626844955318.fasta', 'fasta')                                                                              
-                 ]
-    if kind == 'csv':          
+    # *********** loading sequences *************
+    if kind == 'addH3N2':
+        sequences = [s for s                 
+                     in SeqIO.parse('data/2000_2014_H3N2_global_626844955318.fasta', 'fasta')                                                                              
+                     ]
+    elif kind == 'pH1N1':          
+        sequences = [s for s                 
+                     in SeqIO.parse('data/2000_2018-pH1N1-global_87279350985.fasta', 'fasta')                                                                              
+                     ]    
+    elif kind == 'xpH1N1':          
+        sequences = [s for s                 
+                     in SeqIO.parse('data/2000_2018-xpH1N1-global_263410394807.fasta', 'fasta')                                                                              
+                     ]            
+    else:
+        sequences = [s for s
+                     in SeqIO.parse('data/20170531-H3N2-global.fasta', 'fasta')                            
+                     ]  
+        
+        
+    # *********** loading metadata *************
+    if kind == 'addH3N2':          
         metadata = pd.read_csv('data/2000_2014_H3N2_global_589817715540-Results.tsv',
                                sep='\t',
                                parse_dates=['Collection Date'])
+    elif kind == 'pH1N1': 
+        metadata = pd.read_csv('data/2000_2018-pH1N1-global_287558903998.tsv',        
+                               sep='\t',
+                               parse_dates=['Collection Date'])         
+    elif kind == 'xpH1N1': 
+        metadata = pd.read_csv('data/2000_2018-xpH1N1-global_296089903487.tsv',        
+                               sep='\t',
+                               parse_dates=['Collection Date'])            
+    elif kind == 'csv': 
+        metadata = pd.read_csv('data/20170531-H3N2-global.tsv',        
+                               sep='\t',
+                               parse_dates=['Collection Date'])                  
     elif kind == 'feather':
         metadata = pd.read_feather('data/20170531-H3N2-global.feather')        
+        
     endtime = datetime.now()
     elapsed = endtime - starttime
     print(f'load_sequence_and_metadata() took {elapsed} seconds.')
